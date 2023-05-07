9229fc6c08aaf356a60450693389a2494801b543
912-dobritoiu-davidconstantin <91673180+912-dobritoiu-davidconstantin@users.noreply.github.com>
2023-05-07 18:32:11 +0300

    update

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const TOKEN_KEY = 'auth_token';
+
 class AuthService {
   register(signUpRequest) {
     return axios.post("/api/register", signUpRequest);
@@ -10,11 +12,30 @@ class AuthService {
   }
 
   login(loginRequest) {
-    return axios.post("/api/signin", loginRequest);
+    return axios.post("/api/signin", loginRequest)
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem(TOKEN_KEY, response.data.accessToken);
+        }
+
+        return response.data;
+      });
   }
 
   logout() {
+    localStorage.removeItem(TOKEN_KEY);
     return axios.post("/api/signout");
+  }
+
+  getCurrentUser() {
+    const token = localStorage.getItem(TOKEN_KEY);
+
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.sub;
+    }
+
+    return null;
   }
 }
 
