c69ffc7df2f62a8a343e02d8c0aceedb9670b50b
912-dobritoiu-davidconstantin <91673180+912-dobritoiu-davidconstantin@users.noreply.github.com>
2023-05-14 19:42:57 +0300

    update

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -5,16 +5,16 @@ import axios from 'axios';
 const UserProfile = ({ roles }) => {
   const { id } = useParams();
   const [user, setUser] = useState({
-    bio: "",
-    location: "",
-    birthdate: "",
-    gender: "",
-    maritalStatus: ""
+    bio: null,
+    location: null,
+    birthdate: null,
+    gender: null,
+    maritalStatus: null
   });
 
   useEffect(() => {
     fetchUser();
-  });
+  }, []);
 
   const fetchUser = () => {
     axios
@@ -27,11 +27,11 @@ const UserProfile = ({ roles }) => {
     <div>
       <h1>User Profile</h1>
       <p>User ID: {id}</p>
-      <p>Bio: {user.bio}</p>
-      <p>Location: {user.location}</p>
-      <p>Date of Birth: {user.birthdate}</p>
-      <p>Gender: {user.gender}</p>
-      <p>Marital Status: {user.maritalStatus}</p>
+      <p>Bio: {user.bio || 'No bio available'}</p>
+      <p>Location: {user.location || 'Unknown location'}</p>
+      <p>Date of Birth: {user.birthdate || 'Unknown birthdate'}</p>
+      <p>Gender: {user.gender || 'Unknown gender'}</p>
+      <p>Marital Status: {user.maritalStatus || 'Unknown marital status'}</p>
     </div>
   );
 };
