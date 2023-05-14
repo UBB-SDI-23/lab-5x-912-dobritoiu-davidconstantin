516b71ae64922a78478ea9817b88877caf7ec921
912-dobritoiu-davidconstantin <91673180+912-dobritoiu-davidconstantin@users.noreply.github.com>
2023-05-14 19:07:20 +0300

    update

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,61 +1,17 @@
-import React, { useState, useEffect, useCallback } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-function UserProfile() {
+const UserProfile = ({ roles }) => {
   const { id } = useParams();
-  console.log(id);
-  const [user, setUser] = useState({});
 
-  const fetchUser = useCallback(() => {
-    axios
-      .get(`/api/user-profile-id/${id}`)
-      .then((response) => {
-        setUser(response.data);
-      })
-  }, [id]);
-
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+  // Your component logic goes here
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
-          <p>
-            <strong>Bio:</strong> {user.bio}
-          </p>
-        </div>
-        <div className="col-6">
-          <p>
-            <strong>Location:</strong> {user.location}
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <p>
-            <strong>Date of Birth:</strong> ${user.birthdate}
-          </p>
-        </div>
-        <div className="col-6">
-          <p>
-            <strong>Gender:</strong> {user.gender}/5
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <p>
-          <strong>Marital Status:</strong> ${user.maritalStatus}
-        </p>
-      </div>
+    <div>
+      <h1>User Profile</h1>
+      <p>User ID: {id}</p>
+      {/* Render the user profile based on the ID and roles */}
     </div>
   );
-}
+};
 
 export default UserProfile;
