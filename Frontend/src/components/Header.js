b7e06db06afd11d1430d299a6992b7a6234f8ea2
912-dobritoiu-davidconstantin <91673180+912-dobritoiu-davidconstantin@users.noreply.github.com>
2023-05-15 18:21:54 +0300

    update

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -9,41 +9,34 @@ function Header(props) {
   return (
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <Link reloadDocument className="nav-link" to="/">
+        <Link className="nav-link" to="/">
           Home
         </Link>
       </li>
       <li className="nav-item">
         {!isAuthenticated && (
-          <Link reloadDocument className="nav-link" to="/login">
+          <Link className="nav-link" to="/login">
             Login
           </Link>
         )}
       </li>
       <li className="nav-item">
-        {!isAuthenticated && (
-          <Link reloadDocument className="nav-link" to="/register">
-            Register
-          </Link>
-        )}
-      </li>
-      <li className="nav-item">
         {isAuthenticated && roles && roles.includes("ROLE_ADMIN") && (
-          <Link reloadDocument className="nav-link" to="/dashboard">
+          <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
         )}
       </li>
       <li className="nav-item">
         {isAuthenticated && (
-          <Link reloadDocument className="nav-link" to={`/profile/${id}`}>
+          <Link className="nav-link" to={`/profile/${id}`}>
             Profile
           </Link>
         )}
       </li>
       <li className="nav-item">
         {isAuthenticated && (
-          <Link reloadDocument className="nav-link" to="/logout">
+          <Link className="nav-link" to="/logout">
             Logout
           </Link>
         )}
