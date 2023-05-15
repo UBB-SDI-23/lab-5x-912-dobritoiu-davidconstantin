a99c7287b40e340ed4a5ce5b99a9375e2c0f07a7
912-dobritoiu-davidconstantin <91673180+912-dobritoiu-davidconstantin@users.noreply.github.com>
2023-05-15 18:27:07 +0300

    update

--- Frontend/src/components/auth/Logout.js
+++ Frontend/src/components/auth/Logout.js
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AuthService from "../../services/AuthService";
 
 function Logout(props) {
   const isAuthenticated = props.isAuthenticated;
+  const history = useNavigate();
 
   useEffect(() => {
     async function logout() {
       if (isAuthenticated) {
         await AuthService.logout();
+        history.push("/");
       }
     }
 
     logout();
-  });
+  }, [isAuthenticated, history]);
 
   if (!isAuthenticated) {
     window.location.href = "/";
